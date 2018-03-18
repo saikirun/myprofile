2aaef03c7461dede83b8a03fd12e69fd2fb82027
sgutti <shanmukeshwara.gutti@gmail.com>
2018-03-18 23:57:37 +0530

    Added the following -  tomcat plugin. -  myfaces -  database schema and flyway plugin

---
+++ src/main/java/com/myprofile/web/login/LoginCntr.java
@@ -0,0 +1,29 @@
+/**
+ * All Rights Reserved. Private and Confidential. May not be disclosed without
+ * permission.
+ */
+package com.myprofile.web.login;
+
+/**
+ * @author Shanmu
+ * @date Mar 18, 2018 11:00:18 PM
+ */
+public class LoginCntr {
+    // --------------------------------------------------------------- Constants
+    // --------------------------------------------------------- Class Variables
+    // ----------------------------------------------------- Static Initializers
+    // ------------------------------------------------------ Instance Variables
+    // ------------------------------------------------------------ Constructors
+    /**
+     * Create a new <code>LoginCntr</code>
+     */
+    public LoginCntr() {
+       super();
+    }
+    // ---------------------------------------------------------- Public Methods
+    // ------------------------------------------------------- Protected Methods
+    // --------------------------------------------------------- Default Methods
+    // --------------------------------------------------------- Private Methods
+    // ---------------------------------------------------------- Static Methods
+    // ----------------------------------------------------------- Inner Classes
+}
